84b5667c4a95c5967d34f6c96cf9eed34ef5e82d
Roberto Rossa <rcrossa@hotmail.com>
2024-06-14 10:39:04 -0300

    Swagger added,

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from app.views import *
+from flask_swagger_ui import get_swaggerui_blueprint
 
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
 app = Flask(__name__)
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 app.route('/', methods=['GET'])(index)
 app.route('/data', methods=['GET'])(getInformation)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
