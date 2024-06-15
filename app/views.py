ef85c0ad891edf4cec795cb6580466051de09fc5
Roberto Rossa <rcrossa@hotmail.com>
2024-06-14 21:24:31 -0300

    code fixed

--- app/views.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-
-def index():
-    return 'Hello World!'
-
-def getInformation():
-    prueba = [{"id": 1, "name": "Juan"}, {"id": 2, "name": "Pedro"}]
-    return jsonify(prueba)
