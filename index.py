b9ab2bdf8e9690d12add5502d4587620185e8169
Roberto Rossa <rcrossa@hotmail.com>
2024-06-25 11:19:44 -0300

    I added cors.

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
 from app.routes.routes import route
 from static import swagger
@@ -9,6 +10,6 @@ app = Flask(__name__)
 app.register_blueprint(swagger.swagger_ui_blueprint, url_prefix=swagger.SWAGGER_URL)
 app.register_blueprint(route)
 
-
+CORS(app)
 if __name__ == '__main__':
     app.run(debug=True)
