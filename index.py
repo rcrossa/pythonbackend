b8c783bc5e4684ec8fa308292eadaa2b16746ca8
Roberto Rossa <rcrossa@hotmail.com>
2024-06-26 18:39:24 -0300

    Cors fixed.

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ CORS(
             "origins": [
                 "http://localhost",
                 "http://codoacodo.duckdns.org:5000",
+                "http://127.0.0.1:5000",
             ]
         }
     },
