488097a5b1dd628ed7a0041b985c11ff6d443d30
Roberto Rossa <rcrossa@hotmail.com>
2024-06-26 21:25:51 -0300

    Fix.

--- index.py
+++ index.py
@@ -18,6 +18,7 @@ CORS(
                 "http://localhost",
                 "http://codoacodo.duckdns.org:5000",
                 "http://127.0.0.1:5000",
+                "https://main--starlit-mochi-82dea3.netlify.app",
             ]
         }
     },
